c4409715717df50a9882b19cbbff79920e553c13
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-12-03 23:26:03 +0200

    car-client

--- app/catalog/[id]/Car.client.tsx
+++ app/catalog/[id]/Car.client.tsx
@@ -8,28 +8,44 @@ import css from "./Car.module.css";
 import Image from "next/image";
 import { getCarById } from "@/lib/api/clientApi";
 import BookForm from "@/components/BookForm/BookForm";
+// import { Car } from "@/types/car";
 
+// interface Props{
+//   car:Car
+// }
 const CarClient = () => {
   const { id } = useParams<{ id: string }>();
 
-  const { data, isLoading, error } = useQuery({
+  const {
+    data: car,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ["car", id],
     queryFn: () => getCarById(id),
     refetchOnMount: false,
   });
 
-  console.log(data);
+  console.log(car);
 
   if (isLoading) return <p>Loading, please wait...</p>;
 
-  if (error || !data) return <p>Something went wrong.</p>;
+  if (error || !car) return <p>Something went wrong.</p>;
+  const cityCountry = car.address.split(", ").slice(-2).join(", ");
+
+  const minCond = car.rentalConditions.find((c) =>
+    c.toLowerCase().includes("minimum")
+  );
+  const age = minCond?.match(/\d+/)?.[0] ?? null;
+
+  const deposit = car.rentalConditions[2] ?? "";
 
   return (
     <div className={css.carContainer}>
       <div className={css.leftWrapper}>
         <div className={css.imageWrapper}>
           <Image
-            src={data.img}
+            src={car.img}
             alt="Photo of car"
             width={640}
             height={512}
@@ -44,73 +60,77 @@ const CarClient = () => {
 
       <div className={css.rightWrapper}>
         <div className={css.titleIdWrapper}>
-        <h2>Buick Enclave, 2008</h2>
-        <p>Id: 9582</p>
+          <h2>
+            {car.brand} {car.model}, {car.year}
+          </h2>
+          <p>Id: {car.id}</p>
         </div>
         <div className={css.iconCityMileageWrapper}>
-         <svg className={css.icon}>
-          <use href={`/sprite.svg#icon-Location`} />
-        </svg>
-        <p className={css.city}>Kyiv, Ukraine</p>
-        <p>Mileage: 5 858 km</p>
+          <svg className={css.icon}>
+            <use href={`/sprite.svg#icon-Location`} />
+          </svg>
+          <p className={css.city}>{cityCountry}</p>
+          <p>Mileage: {car.mileage} km</p>
         </div>
-        <p className={css.rentalPrice}>40$</p>
-        <p className={css.descrText}>The Buick Enclave is a stylish and spacious SUV known for its comfortable ride and luxurious features.</p>
-        
+        <p className={css.rentalPrice}>{car.rentalPrice}$</p>
+        <p className={css.descrText}>{car.description}</p>
+
         <p className={css.descrTitle}>Rental Conditions: </p>
         <ul className={css.list}>
-          <li className={css.item}>
-            <svg className={css.icon}>
-          <use href={`/sprite.svg#icon-check-circle`} />
-        </svg>
-        <p className={css.bullets}>Minimum age : 25</p>
-          </li>
-          <li className={css.item}>
-            <svg className={css.icon}>
-          <use href={`/sprite.svg#icon-check-circle`} />
-        </svg>
-        <p>Security deposite required </p>
-          </li>
-          <li className={css.item}>
-            <svg className={css.icon}>
-          <use href={`/sprite.svg#icon-check-circle`} />
-        </svg>
-        <p>Valid driver`s license</p>
-          </li>
+          {car.rentalConditions.map((condition, index) => {
+            return (
+              <li className={css.item} key={index}>
+                <svg className={css.icon}>
+                  <use href={`/sprite.svg#icon-check-circle`} />
+                </svg>
+                <p className={css.bullets}>{condition}</p>
+              </li>
+            );
+          })}
         </ul>
         <p className={css.descrTitle}>Car Specifications: </p>
         <ul className={css.list}>
           <li className={css.item}>
             <svg className={css.icon}>
-          <use href={`/sprite.svg#icon-Group`} />
-        </svg>
-        <p>Year: 2008</p>
-        </li>
-        <li className={css.item}>
+              <use href={`/sprite.svg#icon-Group`} />
+            </svg>
+            <p>Year: 2008</p>
+          </li>
+          <li className={css.item}>
             <svg className={css.icon}>
-          <use href={`/sprite.svg#icon-Group1`} />
-        </svg>
-        <p>Type: Suv</p>
+              <use href={`/sprite.svg#icon-Group1`} />
+            </svg>
+            <p>Type: Suv</p>
           </li>
 
           <li className={css.item}>
             <svg className={css.icon}>
-          <use href={`/sprite.svg#icon-Group2`} />
-        </svg>
-        <p>Fuel Consumption: 10.5</p>
+              <use href={`/sprite.svg#icon-Group2`} />
+            </svg>
+            <p>Fuel Consumption: 10.5</p>
           </li>
 
           <li className={css.item}>
             <svg className={css.icon}>
-          <use href={`/sprite.svg#icon-gear`} />
-        </svg>
-        <p>Engine Size: 3.6L V6</p>
+              <use href={`/sprite.svg#icon-gear`} />
+            </svg>
+            <p>Engine Size: 3.6L V6</p>
           </li>
         </ul>
-        
+
         <p className={css.descrTitle}>Accessories and functionalities: </p>
         <ul className={css.list}>
-          <li className={css.item}>
+          {[...car.accessories, ...car.functionalities].map((item, index) => {
+            return (
+              <li className={css.item} key={index}>
+                <svg className={css.icon}>
+                  <use href={`/sprite.svg#icon-check-circle`} />
+                </svg>
+                <p>{item}</p>
+              </li>
+            );
+          })}
+          {/* <li className={css.item}>
             <svg className={css.icon}>
           <use href={`/sprite.svg#icon-check-circle`} />
         </svg>
@@ -145,13 +165,10 @@ const CarClient = () => {
           <use href={`/sprite.svg#icon-check-circle`} />
         </svg>
         <p>Premium audio system</p>
-          </li>
-
-          </ul>
-
-        </div>
+          </li> */}
+        </ul>
       </div>
-    
+    </div>
   );
 };
 export default CarClient;
